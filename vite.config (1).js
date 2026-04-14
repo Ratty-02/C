e2f7c356edb72fe337485fc011970455248558ef
Ratty-02 <rattyatyourservice@gmail.com>
2026-04-15 01:26:25 +0530

    Added complete website

---
+++ vite.config (1).js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                products: 'products/index.html',
+                about: 'about/index.html',
+                contact: 'contact/index.html',
+                blog: 'blog/index.html',
+            }
+        }
+    }
+});
